d451fedcb0d09f03d68aa87b31d32ba878338f52
Gregory <info@nomoregrapes.com>
2014-07-24 20:43:13 +0100

    Missed some bits.

---
+++ web/general.js
@@ -0,0 +1,9 @@
+
+apibase = "http://www.livingwithdragons.com/maps/newday";
+
+$(document).ready(function(){
+	$('.topmenu #home').click(function() {
+		window.location.href = "index.html";
+	});
+});
+
